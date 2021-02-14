8b6402b39677840049bda282cb59a212028552de
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-14 19:00:56 -0300

    added socket.io

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const socketio = require('socket.io');
 const router = require('./routes');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(router);
 
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketio(server, { cors: { origin: 'http://localhost:3000' } });
 
-module.exports = {
-  server,
-  io,
-};
+// const users = [];
+
+io.use((socket, next) => {
+  const username = socket.handshake.auth.username;
+  if (!username) {
+    return next(new Error('invalid username'));
+  }
+  socket.username = username;
+  next();
+});
+
+module.exports = { io, server };
