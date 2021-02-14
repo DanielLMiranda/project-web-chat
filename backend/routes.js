b63c93b3400770b2910cb66a7461e8c3e4f4d9c8
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-14 19:01:21 -0300

    created users routes

--- backend/routes.js
+++ backend/routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
+const Controllers = require('./controllers');
 
-router.get('/', (_req, res) => {
-  res.send('Hello World!');
-});
+router.get('/users', Controllers.User.findUsers);
+router.get('/users/:userId/contacts', Controllers.Contact.findContacts);
+router.post('/users', Controllers.User.createUser);
+router.post('/users/:userId/contacts', Controllers.Contact.createContact);
 
 module.exports = router;
