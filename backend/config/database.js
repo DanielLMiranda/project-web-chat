05a666dea210e40703a0f62bd578ad67f6715a9d
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-17 21:17:09 -0300

    changed info to use .env

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,26 +1,14 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: 'postgres',
-    password: null,
-    database: 'hey',
-    host: '127.0.0.1',
+    username: process.env.POSTGRES_USERNAME,
+    password: process.env.POSTGRES_PASSWORD,
+    database: process.env.POSTGRES_DATABASE,
+    host: process.env.POSTGRES_HOST,
     dialect: 'postgres',
     define: {
       timestamps: true,
     },
-  },
-  test: {
-    username: 'root',
-    password: null,
-    database: 'database_test',
-    host: '127.0.0.1',
-    dialect: 'mysql',
-  },
-  production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
   },
 };
