a18b8bc7af798fd5205d0f15997ae838720074df
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-17 21:18:24 -0300

    added new session and user routes

--- backend/routes.js
+++ backend/routes.js
@@ -1,9 +1,22 @@
 const router = require('express').Router();
 const Controllers = require('./controllers');
+const authMiddleware = require('./middlewares/auth');
 
-router.get('/users', Controllers.Users.findUsers);
-router.get('/users/:userId/contacts', Controllers.UserContacts.findContacts);
+// Unauthorized routes
+// Users
 router.post('/users', Controllers.Users.createUser);
+
+router.use(authMiddleware);
+
+// Session
+router.post('/sessions', Controllers.Session.createSession);
+
+// Users
+router.get('/users', Controllers.Users.findUsers);
+router.put('/users', Controllers.Users.updateUser);
+
+// Contacts
+router.get('/users/:userId/contacts', Controllers.UserContacts.findContacts);
 router.post('/users/:userId/contacts', Controllers.UserContacts.createContact);
 
 module.exports = router;
