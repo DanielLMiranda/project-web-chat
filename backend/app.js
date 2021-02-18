85676c145c3bd3b5829315aaf2a835f1ac1f78b9
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-17 21:13:13 -0300

    added dotenv import

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const http = require('http');
 const socketio = require('socket.io');
 const router = require('./routes');
+require('dotenv').config();
 
 const app = express();
 app.use(express.json());
@@ -11,16 +12,5 @@ app.use(router);
 
 const server = http.createServer(app);
 const io = socketio(server, { cors: { origin: 'http://localhost:3000' } });
-
-// const users = [];
-
-// io.use((socket, next) => {
-//   const username = socket.handshake.auth.username;
-//   if (!username) {
-//     return next(new Error('invalid username'));
-//   }
-//   socket.username = username;
-//   next();
-// });
 
 module.exports = { io, server };
