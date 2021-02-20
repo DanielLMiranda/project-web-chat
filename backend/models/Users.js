1a0cc80d03c1a77507876ab798bc57a25f290b19
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-19 21:08:18 -0300

    added password verification

--- backend/models/Users.js
+++ backend/models/Users.js
@@ -2,7 +2,11 @@ const bcrypt = require('bcryptjs');
 
 const createUsers = (sequelize, DataTypes) => {
   const Users = sequelize.define('Users', {
-    userId: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+    userId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     userName: DataTypes.STRING,
     phone: DataTypes.STRING,
     password: DataTypes.VIRTUAL,
@@ -14,6 +18,10 @@ const createUsers = (sequelize, DataTypes) => {
       user.passwordHash = await bcrypt.hash(user.password, 8);
     }
   });
+
+  Users.prototype.checkPassword = function checkPassword(password) {
+    return bcrypt.compare(password, this.passwordHash);
+  };
 
   return Users;
 };
