7d76400e377905a3e07bdcb9ebde04d3e3437b83
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-19 21:08:40 -0300

    added routes component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Routes from './routes';
 import ChatProvider from './context/Chat';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <ChatProvider>
-      <Router>
-        <Switch>
-          <Route path="/home">
-            <HomePage />
-          </Route>
-          <Route path="/">
-            <LoginPage />
-          </Route>
-        </Switch>
-      </Router>
+      <Routes />
     </ChatProvider>
   );
 }
