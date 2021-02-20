11fd1199ac18899724b1004af5512b8a5ebd7794
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2021-02-19 21:10:11 -0300

    added basic login page interface

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
+import styles from './LoginPage.module.css';
 
 const LoginPage = () => {
   const [phone, setPhone] = React.useState('');
   const [password, setPassword] = React.useState('');
 
-  console.log(phone, setPhone, password, setPassword);
+  const handleSubmit = () => {
+    console.log('Ok');
+  };
 
-  return <div>Test</div>;
+  return (
+    <div id={styles.pageContainer}>
+      <h1>Hey</h1>
+      <form onSubmit={handleSubmit} id={styles.loginForm}>
+        <input
+          type="text"
+          value={phone}
+          onChange={({ target }) => setPhone(target.value)}
+          placeholder="Phone number"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+          placeholder="Password"
+        />
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
